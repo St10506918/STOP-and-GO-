eeee28175515a691796562c325aca7334c6e540f
Joshua McAdam <st10506918@myemeris.edu.za>
2026-02-24 12:35:07 +0200

    First submission using the CLM

---
+++ src/the/beginning/TheBeginning.java
@@ -0,0 +1,34 @@
+/************************************************************************
+* This is the STOP and GO sign activity that I did in class.            *
+*************************************************************************/
+
+package the.beginning;
+
+/**
+ *
+ * @author Joshua McAdam
+ */
+public class TheBeginning {
+
+    
+    public static void main(String[] args) {
+        
+        
+        //Stop sign Activity.
+        //The alignment involves the usage of escape sequences. 
+        //The figures won't appear as they are due to the use of escape sequences.
+        System.out.println("\n\n\n\t\t ******** \t\t\tXXXXXXXX");
+        System.out.println("\t      *   \t   * \t\t     x \t\t  x");
+        System.out.println("\t     * \t   STOP     *     \t   X       GO\t    X");
+        System.out.println("\t      *   \t   * \t\t     x \t\t  x");
+        System.out.println("\t\t ******** \t\t\tXXXXXXXX");
+        System.out.println("\t\t    ** \t\t\t\t   XX");
+        System.out.println("\t\t    ** \t\t\t\t   XX");
+        System.out.println("\t\t    ** \t\t\t\t   XX");
+        System.out.println("\t\t    ** \t\t\t\t   XX");
+        System.out.println("\t\t   **** \t\t\t  XXXX ");
+        System.out.println("\t\t********** \t\t       XXXXXXXXXX");
+        
+    }
+    
+}
